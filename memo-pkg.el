61a98e125db6e819cb69e9e884501bb58f7a857b
Mark W. Naylor <mark.naylor.1701@gmail.com>
2017-12-18 01:27:50 -0500

    Turn into an Emacs package

---
+++ memo-pkg.el
@@ -0,0 +1,10 @@
+;; author: Mark W. Naylor
+;; file:  memo-pkg.el
+;; date:  2017-Dec-17
+
+(define-package
+  "memo"
+  "0.9"
+  "Interactively creates a memorandum."
+  '((s "20171102.227"))
+  :keywords '("office" "memorandum"))
--- memo.el
+++ memo.el
@@ -1,80 +1,88 @@
-;; author: Mark W. Naylor
-;; file:  memo.el
-;; date:  2017-May-03
+;;; memo.el --- Interactive creation of memoranda.
 
-;; Package for automating creation of memoranda.
+;; Copyright (©) 2017  Mark W. Naylor <mark.naylor.1701@gmail.com>
 
+;; Author: Mark W. Naylor
+;; Version: 0.9
+;; Package-Requires: ((s "2017102.227"))
+;; Keywords: office memorandum
+;; Created:  2017-May-03
+
+;;TODO:
+;;; Assign suitable copyright notice. (Probably a BSD version).
+
+(require 's)
 
 (setq lexical-binding t)
 
-(defun memo/generate ()
+(defun memo-generate ()
   (interactive)
   (find-file (read-string "memo file name:  " "memo.txt"))
 
-  (insert (s-join "\n" (append (mapcar #'memo/formatted-item (memo/items)) '(""))))
+  (insert (s-join "\n" (append (mapcar #'memo--formatted-item (memo--items)) '(""))))
   (newline 2)
   (insert "MESSAGE BODY"))
 
-(defun memo/items-base ()
+(defun memo--items-base ()
       (list
-       (list "TO" (list "To" #'memo/recipient))
+       (list "TO" (list "To" #'memo--recipient))
        (list "FROM" (list "From" #'user-full-name))
-       (list "SUBJ" (list "Subject" #'memo/subject))
+       (list "SUBJ" (list "Subject" #'memo--subject))
        (list "DATE" (list "Date" #'short-date))))
 
-(defun memo/items ()
-  (let ((items (memo/items-base)))
+(defun memo--items ()
+  (let ((items (memo--items-base)))
     (-zip
-     (memo/paddify (mapcar #'memo/colon (mapcar #'car items)))
+     (memo--paddify (mapcar #'memo--colon (mapcar #'car items)))
      (mapcar #'cadr items))))
 
-(defun memo/formatted-item (memo-item)
+(defun memo--formatted-item (memo-item)
   "docstring"
   (let ((label (car memo-item))
         (prompt
          (s-pad-right
-            (memo/padding (length (cadr memo-item)))
+            (memo--padding (length (cadr memo-item)))
             " "
             (cadr memo-item)))
         (fn (caddr memo-item)))
     (concat label (read-string prompt (funcall fn)))))
 
 
-(defun memo/longest (lst)
+(defun memo--longest (lst)
   (apply #'max
          (mapcar #'length lst)))
 
-(defun memo/colon (s)
+(defun memo--colon (s)
   (concat s ":"))
 
-(defun memo/padding  (n)
+(defun memo--padding  (n)
   (+ n 2))
 
-(defun memo/item-jusify (s pad-size)
+(defun memo--item-jusify (s pad-size)
   (s-pad-right pad-size " " s))
 
-(defun memo/paddify (lst)
+(defun memo--paddify (lst)
   (mapcar
-   (lambda (s) (memo/item-jusify s (memo/padding (memo/longest lst))))
+   (lambda (s) (memo--item-jusify s (memo--padding (memo--longest lst))))
    lst))
 
 ;;--------------------------------------------------
-(defun memo/recipient ()
+(defun memo--recipient ()
   "Returns a default recipient string."
   "<recipient>")
 
-(defun memo/subject ()
+(defun memo--subject ()
   "Returns a default subject string."
   "<subject>")
 
 ;;--------------------------------------------------
 
 
-(defun memo/read-string-nil (prompt &optional initial-input history default-value inherit-input-method)
+(defun memo--read-string-nil (prompt &optional initial-input history default-value inherit-input-method)
   (let ((resp (read-string prompt initial-input history default-value inherit-input-method)))
     (when ((not equal) resp "")
       resp)))
 
-;; (defun memo/formatted-item (prompt default prefix reader-fn)
-;;   #'(lambda ()
-;;     (concat prefix (funcall reader-fn prompt default))))
+(provide 'memo)
+
+;;; memo.el ends here
